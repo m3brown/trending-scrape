622b25a929f03712969403823cb69028c0fc182f
Mike Brown <mike.brown@excella.com>
2018-04-22 16:27:40 -0400

    Trim one line of code

--- trending.py
+++ trending.py
@@ -2,8 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 page = requests.get("https://github.com/trending/python?since=monthly")
 soup = BeautifulSoup(page.content, 'html.parser')
-repos_html = soup.select("ol.repo-list li")
-for repo in repos_html:
+for repo in soup.select("ol.repo-list li"):
     repo_link = repo.find('h3').find('a').attrs['href']
     description = repo.select('div.py-1 p')[0].text.strip()
     tab_width = int(round((40 - len(repo_link))/8.0 + .49))
